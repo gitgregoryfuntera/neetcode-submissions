c877f65a28c30fb0ebffc44c89404da99919fcf7
John Gregory Funtera <47694698+gitgregoryfuntera@users.noreply.github.com>
2026-07-19 09:24:02 +0800

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        const mapper: Record<string, number> = {}
+        const highestValues = []
+        for(const item of nums) {
+            const existingItem = mapper[item]
+            if (existingItem === undefined) {
+                mapper[item] = 0
+            } else {
+                mapper[item]= existingItem + 1
+            }
+        }
+        const getValuesFromMapper =  Object.entries(mapper).sort((a, b) => b[1] - a[1]);
+        for(let i = 0; i<k;i++) {
+            const item = Number(getValuesFromMapper[i][0])
+            highestValues.push(item)
+        }
+        return highestValues
+    }
+}
