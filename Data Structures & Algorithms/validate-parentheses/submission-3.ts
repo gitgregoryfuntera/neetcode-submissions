f21c26b91eef4e961d00a7b50a4e8170941a68a0
John Gregory Funtera <47694698+gitgregoryfuntera@users.noreply.github.com>
2026-08-03 19:51:49 +0800

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.ts
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s: string): boolean {
+        const mapper = {
+            '}':'{',
+            ']':'[',
+            ')':'('
+        }
+        const formatArray = s.split('')
+        if (formatArray.length === 1) {
+            return false
+        }
+        const stack = []
+        for(let i = 0; i < formatArray.length; i++) {
+            const item = formatArray[i]
+            if (mapper[item] === undefined) {
+                stack.push(item)
+            } else {
+                const lastStackItem = stack[stack.length - 1]
+                const expectedItem = mapper[item]
+                if (lastStackItem === expectedItem) {
+                    stack.pop()
+                } else {
+                    stack.push(item)
+                }
+            }
+        }
+
+        
+       
+        return stack.length === 0;
+    }
+}
