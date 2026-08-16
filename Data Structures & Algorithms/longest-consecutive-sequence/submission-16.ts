c113e2a98fa8197f7ba55c0ad696039ede238748
John Gregory Funtera <47694698+gitgregoryfuntera@users.noreply.github.com>
2026-08-16 13:32:49 +0800

    Add: longest-consecutive-sequence - submission-16

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-16.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums: number[]): number {
+        const setTable = new Set<number>()
+        for(const item of nums) {
+            setTable.add(item)
+        }
+        let maxValue = 0
+        for(const item of nums) {
+            const prevItem = item - 1
+            if (!setTable.has(prevItem)) {
+                let isRunning = true
+                let checkItem = prevItem
+                let value = 0
+                while (isRunning) {
+                    checkItem++
+                    if (setTable.has(checkItem)) {
+                        value++
+                    } else {
+                        isRunning = false
+                    }
+                }
+                maxValue = Math.max(maxValue, value)
+            }
+        }
+        return maxValue
+    }
+}
