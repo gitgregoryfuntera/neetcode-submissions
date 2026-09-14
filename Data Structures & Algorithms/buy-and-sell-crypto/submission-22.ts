d8c11ed41629bf8a18b91d51b615aaab524e35bc
John Gregory Funtera <47694698+gitgregoryfuntera@users.noreply.github.com>
2026-09-14 09:00:26 +0800

    Add: buy-and-sell-crypto - submission-22

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-22.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+        let minPrice = prices[0]
+        let maxProfit = 0
+        for(let i = 1; i < prices.length; i++) {
+            const futurePrice = prices[i]
+            const profit = futurePrice - minPrice
+            maxProfit = Math.max(profit, maxProfit)
+            minPrice = Math.min(prices[i], minPrice)
+        }
+
+        return maxProfit
+    }
+}
